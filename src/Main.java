0ede3fafda93b77ce2702f6071822e2e5128a1cf
Gustav Vang Klinker <gulle139@hotmail.com>
2018-10-08 12:46:17 +0200

    Gustavs Branch

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main{
     public static void main(String[] args) {
         Player player1 = new Player("Bob");
         Player player2 = new Player("Bobby");
-        Dice dice = new Dice();
+        Dice die1 = new Dice();
+        Dice die2 = new Dice();
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to IOOuterActive dice game");
         System.out.println("To play, press 1 and enter");
@@ -17,22 +18,22 @@ public class Main{
                     System.out.println("An error occurred");
 
                 else if (player1.getIsTurn()) {
-                    dice.roll();
-                    System.out.println(dice.getSum());
-                    dice.addPoints(player1);
-                    if(dice.dieEquals() && dice.getSum()==2)
+                    die1.roll();
+                    System.out.println(die1.getSum(die2));
+                    die1.addPoints(player1);
+                    if(die1.dieEquals() && die1.getSum(die2)==2)
                         player1.setScore(0);
-                    if(dice.dieEquals())
+                    if(die1.dieEquals())
                         System.out.println("Two of a kind gives an extra round");
                     else
-                    player1.turnSwitch(player2);
+                        player1.turnSwitch(player2);
                 } else if (player2.getIsTurn()) {
-                    dice.roll();
-                    System.out.println(dice.getSum());
-                    dice.addPoints(player2);
-                    if(dice.dieEquals() && dice.getSum()==2)
+                    die1.roll();
+                    System.out.println(die1.getSum(die2));
+                    die1.addPoints(player2);
+                    if(die1.dieEquals() && die1.getSum(die2)==2)
                         player2.setScore(0);
-                    if(dice.dieEquals())
+                    if(die1.dieEquals())
                         System.out.println("Two of a kind gives an extra round");
                     else
                         player2.turnSwitch(player1);
