3c1cd834896eb757fb767ff42d36b8be9acbcb17
Gustav Vang Klinker <gulle139@hotmail.com>
2018-10-09 10:16:49 +0200

    Mre overskuelighed i while loopet

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,12 @@ public class Main{
                 if (player1.getIsTurn() && player2.getIsTurn())
                     System.out.println("An error occurred");
 
-                else if (player1.getIsTurn()) {
-                    die1.roll();
-                    die2.roll();
-                    System.out.println(die1.getFaceValue() + " and " + die2.getFaceValue());
+
+                die1.roll();
+                die2.roll();
+                System.out.println(die1.getFaceValue() + " and " + die2.getFaceValue());
+
+                if (player1.getIsTurn()) {
                     player1.addToScore(die1, die2);
                     if(die1.equals(die2) && die1.getSum(die2)==2)
                         player1.setScore(0);
@@ -30,10 +32,8 @@ public class Main{
                         System.out.println("Two of a kind gives an extra round");
                     else
                         player1.turnSwitch(player2);
-                } else if (player2.getIsTurn()) {
-                    die1.roll();
-                    die2.roll();
-                    System.out.println(die1.getFaceValue() + " and " + die2.getFaceValue());
+                }
+                else if (player2.getIsTurn()) {
                     player2.addToScore(die1, die2);
                     if(die1.equals(die2) && die1.getSum(die2)==2)
                         player2.setScore(0);
@@ -46,8 +46,8 @@ public class Main{
             }
             showScore(player1, player2);
             showWinner(player1, player2);
-            if(input == 2)
-                System.out.println(player1.getIsTurn()+ " " +player2.getIsTurn() );
+            /*if(input == 2)
+                System.out.println(player1.getIsTurn()+ " " +player2.getIsTurn() );*/
         }
     }
     public static void showScore(Player player1, Player player2){
